3b8fcaa2b1cef2bcfda1d95cd5017b180fa0db98
samhitapn <samhitapn.96@gmail.com>
2017-04-11 07:15:34 +0200

    Create DataParsing.py

---
+++ DataParsing.py
@@ -0,0 +1,62 @@
+import sys
+import numpy as np
+
+# Initialising variables
+arrayCGHData = ""
+arrayGroups = ""
+cghData = []
+groupDataDict = {}
+compiledData = []
+cghDataList = []
+cghDataDict = {}
+
+# Get the files from the input or use default
+try:
+    arrayCGHData = sys.argv[1]
+    arrayGroups = sys.argv[2]
+except IndexError:
+    print("No data files given. I will use the default files!!")
+    arrayCGHData = "Train_call.txt"
+    arrayGroups = "Train_Clinical.txt"
+
+fileData = open(arrayCGHData, "r")
+fileGroups = open(arrayGroups, "r")
+
+# Get the data from CGH Data file
+for line in fileData:
+D    lineTerm = line.rstrip("\n").split("\t")
+    cghData.append(lineTerm)
+fileData.close()
+
+# Transpose the CGH Data matrix
+cghDataMatrix = np.matrix(cghData)
+cghDataMatrixTrans = cghDataMatrix.transpose()
+cghDataMatrixTrans = np.delete(cghDataMatrixTrans , [1,2,3], 0)
+cghDataList = cghDataMatrixTrans.tolist()
+
+# Store the CGH Data into a dictionary
+for i in range(0,101):
+    a = cghDataList[i][0]
+    array = a.replace('"',"")
+    cghDataDict[array] = cghDataList[i][1:]
+
+# Get the data from the array groups file
+for line in fileGroups:
+    lineTerm = line.rstrip("\n").split("\t")
+    groupDataDict[lineTerm[0]] = lineTerm[1]
+fileGroups.close()
+
+# Compile the final matrix - merge the data from the dictionaries based on Array
+compiledData.append(["Array","Group", *cghDataList[0][1:]])
+for a,v in cghDataDict.items():
+    if "Array" in a:
+        compiledData.append([a, groupDataDict[a],*cghDataDict[a]])
+
+# Print the final List into a file
+outputFile = open("Complied-Data.txt","w")
+for i in compiledData:
+    for j in i:
+        outputFile.write(j)
+        outputFile.write("\t")
+    outputFile.write("\n")
+outputFile.close()
