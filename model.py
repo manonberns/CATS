504ddc0ff83f043711a8970d90377ef0efc0fc79
amberhendriks <ahproefdedag@gmail.com>
2017-04-13 09:06:25 +0200

    Update model.py
    shorter version

--- model.py
+++ model.py
@@ -1,10 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.svm import SVC
-from sklearn.model_selection import StratifiedKFold
-from sklearn.feature_selection import RFECV
-from sklearn.datasets import make_classification
-from sklearn.cross_validation import train_test_split
+import sklearn as sk
 
 #Variables
 TEST_SIZE = 0.1
@@ -21,15 +17,15 @@ def import_data():
 
 # returns train and test set
 def split_data():
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=0)
+    X_train, X_test, y_train, y_test = sk.cross_validation.train_test_split(X, y, test_size=TEST_SIZE, random_state=0)
     return(X_train, X_test, y_train, y_test)
     
 def SVC_crossvalidation(X,y):
     # Create the RFE object and compute a cross-validated score.
-    svc = SVC(kernel="linear")
+    svc = sk.svm.SVC(kernel="linear")
     # The "accuracy" scoring is proportional to the number of correct
     # classifications
-    rfecv = RFECV(estimator=svc, step=1, cv=StratifiedKFold(2), scoring='accuracy')
+    rfecv = sk.feature_selection.RFECV(estimator=svc, step=1, cv=sk.model_selection.StratifiedKFold(2), scoring='accuracy')
     rfecv.fit(X, y)
     print("Optimal number of features : %d" % rfecv.n_features_)
     # Plot number of features VS. cross-validation scores
